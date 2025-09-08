70970a8e8ec4c254b6124868de89af5580aec380
Jennifer Tarleton <jennifertarleton@gmail.com>
2025-09-08 18:30:40 -0500

    Setup for script.js icon btns and save btn

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 console.log("Hello from script.js!");
 
+// Get DOM elements
+const moodOptions = document.getElementById("mood-options");
+const journalEntry = document.getElementById("journal-entry");
+const calendar = document.getElementById("calendar");
+const daysTracked = document.getElementById("days-tracked");
+const streak= document.getElementById("streak");
+const avgMood = document.getElementById("avg-mood");
+
+// Start selected mood at null
+let selectedMood = null;
+
+// Save icon btns
+const moodInputs = document.querySelectorAll(".mood-icon");
+moodInputs.forEach(mood => {
+    mood.addEventListener("click", () => {
+        console.log("Mood Icon Clicked!");
+    })
+})
+
+// Event handler for the save entry button
+const saveEntry = document.getElementById("save-entry");
+saveEntry.addEventListener("click", function() {
+    console.log("Save Btn Clicked!");
+})
